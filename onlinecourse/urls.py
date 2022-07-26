df0f19b5103cdfffaeb02c9e53435a1dacd85ded
ablarer <ablarer@gmx.ch>
2022-07-26 13:50:18 +0000

    after final lab 4

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     # <HINT> Create a route for submit view
     path('<int:course_id>/submit/', views.submit, name='submit'),
     # <HINT> Create a route for show_exam_result view
-    path('course/<int:course_id>/submission/<int:submission_id>/result/', ...),
+    path('<int:result_id>/result/', views.show_exam_result, name='result'),
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # <HINT> Import any new Models here
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Question, Choice, Submission
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from django.contrib.auth import login, logout, authenticate
 import logging
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
+
 # Create your views here.
-
-
 def registration_request(request):
     context = {}
     if request.method == 'GET':
